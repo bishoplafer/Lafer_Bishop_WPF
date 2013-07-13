2d7cd4e77949701f2703b9ebcaa630751a1324e6
Bishop Lafer <bishoplafer@fullsail.edu>
2013-07-12 23:02:27 -0400

    Finished Dog Years Problem

---
+++ Expressions_worksheet/js/scripts.js
@@ -0,0 +1,9 @@
+// Bishop Lafer
+// July 12, 2013
+// Expressions Worksheet
+
+// Dogs age seven times faster than humans. If Sparky is 4 years old in human years how old is he in dog years?
+var humanYears = 4; // declare humanYears variable and define it as 4 (This is Sparky's age in human years
+var dogYears = humanYears * 7; // declare dogYears variable and define it as 7 times humanYears (This is Sparky's age in dog years)
+console.log("Sparky is " + humanYears + " years old, which is " + dogYears + " in dog years");
+
