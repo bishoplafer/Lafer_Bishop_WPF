4a043da92e5c6f41eca0581cb0d152cc4fb10244
Bishop Lafer <bishoplafer@fullsail.edu>
2013-07-14 00:00:24 -0400

    Added Date for Loan Terms

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,7 +7,7 @@
 var itemMan = prompt("Please input item Manufacturer"); // declare itemMan variable and prompt user to input manufacturer
 console.log("Manufacturer = " + itemMan);
 var itemMod = prompt("Please input item Model"); // declare itemMod variable and prompt user to input model
-console.log("Mode = " + itemMod);
+console.log("Model = " + itemMod);
 var retailPrice = prompt("Please input New Retail Price"); // declare retailPrice variable and define it
 console.log("Retail Price = $" + retailPrice);
 // Determine Resale Price by multiplying retailPrice by .65 (65%)
@@ -21,9 +21,47 @@ console.log("Resale Price = $" + resalePrice.toFixed(2)); // show resalePrice ro
 var purchasePrice = resalePrice / 1.68;  // determine purchasePrice
 console.log("Purchase Price = $" + purchasePrice.toFixed(2)); // show purchasePrice
 
+// Get the Date for Loan terms
+var months = ["January","February","March","April","May","June","July","August","September","November","December"];
+
+var dateNow = new Date();
+var yearNow = dateNow.getFullYear();
+var monthNow = months[dateNow.getMonth()];
+var dayNow = dateNow.getDate();
+var daySuffix;
+
+switch (dayNow)
+{
+    case 1:
+    case 21:
+    case 31:
+        daySuffix = "st";
+        break;
+    case 2:
+    case 22:
+        daySuffix = "nd";
+        break;
+    case 3:
+    case 23:
+        daySuffix = "rd";
+        break;
+    default:
+        daySuffix = "th";
+        break;
+}
+
+console.log(monthNow + " " + dayNow + daySuffix + " " + yearNow);
+
+var loanStart = dateNow.getDate();
+var thirtyDays = dateNow.setDate(loanStart + 30);
+var sixtyDays = dateNow.setDate(loanStart + 60);
+
 var loanInterest = .25 * purchasePrice; // Pawn law in Florida is 25% interest every 30 days. Default loan period = 60 Days
 var pickUpPrice = purchasePrice + loanInterest; // Customers pay the original loan + 25% interest to get their items back
-
+var thirtyDays = pickUpPrice;
+var sixtyDays = purchasePrice + loanInterest * 2;
+console.log("$" + thirtyDays.toFixed(2) + " to pick up in the first 30 days or $" + sixtyDays.toFixed(2) + " in the second 30 days.");
 
 alert("The Resale Value for the " + itemMan + " " + itemMod + " is $" + resalePrice.toFixed(2));
 alert("The Purchase Price for the " + itemMan + " " + itemMod + " is $" + purchasePrice.toFixed(2));
+alert("$" + thirtyDays.toFixed(2) + " to pick up in the first 30 days or $" + sixtyDays.toFixed(2) + " in the second 30 days.");
