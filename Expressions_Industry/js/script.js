c25482cec73d5b673e6ba06c5b5a7816b1bb7b14
Bishop Lafer <bishoplafer@fullsail.edu>
2013-07-16 18:03:36 -0400

    Expressions_Industry completed
    Wrote script for Expressions_Industry including all comments

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,13 @@
+// Bishop Lafer
+// July 16, 2013
+// Expressions_Industry
+
+// Calculate how many pixels square an image resolution is based on dpi, width and height
+var dpi = prompt("Please input the image dpi"); // declare dpi variable and define it as user input
+var width = prompt("Please input the image width"); // declare width variable and define it as user input
+var height = prompt("Please input the image height"); // declare height variable and define it as user input
+
+var pixelSq = parseInt(width) * parseInt(height) / parseInt(dpi); // declare pixelSq and set it equal to the width * height / dpi
+console.log("The image is " + pixelSq + " pixels squared."); // write to console how many pixelSq the image is
+
+
