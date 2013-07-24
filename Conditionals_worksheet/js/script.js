af86277938f72f91bdedaa4253961afe4aa77bda
Bishop Lafer <bishoplafer@fullsail.edu>
2013-07-23 23:10:47 -0400

    Fixed Comments
    Fixed Comments on Conditionals Worksheet

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -16,18 +16,21 @@ var contestMinWeight = 250;
 var degrees = new Array(32, 100, 90); // new array with degrees test data set
 var unit = new Array("F", "C", "F"); // new array with temperature scale for degrees
 var conversion = new Array(); // new array for temperature conversions
+// loopCounter = index of Array 
 var loopCounter; // variable loopcounter to allow me to test each element in each array
 
-// for the index(loopcounter) 0,1,2 do these things:
+// for the index(loopCounter) start at 0 and increase by 1 until index(loopCounter) = 2 do these things:
 for (loopCounter = 0; loopCounter <= 2; loopCounter++) 
 {
-	// if unit at index is in Fahrenheit convert it to Celsius and print the conversion to the console
+	// if var unit at index(loopCounter) is in Fahrenheit, var conversion at index(loopCounter) is var degrees at index(loopCounter) converted to Celsius
 	if(unit[loopCounter] === "F"){
 		conversion[loopCounter] = 5 / 9 * (degrees[loopCounter] - 32);
+		// print the conversion to console
 		console.log(degrees[loopCounter] + unit[loopCounter] + " is " + conversion[loopCounter] + "C.");
-		// if unit at index is Celsius convert it to Fahrenheit and print the conversion to the console
+		// if var unit at index(loopCounter) is Celsius, var conversion at index(loopCounter) is var degrees at index(loopCounter) converted to Fahrenheit
 	}else{if(unit[loopCounter] === "C"){
 			conversion[loopCounter] = 9 / 5 * degrees[loopCounter] + 32;
+			// print the conversion to console
 			console.log(degrees[loopCounter] + unit[loopCounter] + " is " + conversion[loopCounter] + "F.");
 		}
 	}
