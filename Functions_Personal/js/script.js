eb37200478b95369b9809d6eed4c6bfff7e2b1d5
Bishop Lafer <bishoplafer@fullsail.edu>
2013-07-28 00:51:14 -0400

    Functions_Personal
    Printed information collected to console in organized fashion

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -13,11 +13,13 @@ var newCustomer = function(){
 	birthDay.setDate(bDay);
 	birthDay.setMonth(bMonth);
 	birthDay.setFullYear(bYear);
+	customerBirthDay = birthDay.getMonth()+"/"+birthDay.getDate()+"/"+birthDay.getFullYear();
 	sex = prompt("Is the customer Male or Female?","M or F");
 	race = prompt("What is the Customer's Race?\n(W)White, (B)Black, (H)Hispanic, (A)Asian, (O)Other","");
 	heightFeet = prompt("How many feet tall is the customer?","");
 	heightInches = prompt("How many inches?","");
 	height = heightFeet + "'" + heightInches + '"';
+	weight = parseInt(prompt("How much does the Customer weigh?", ""));
 	phone = prompt("What is the Customer's Phone Number?", "(XXX) XXX-XXXX");
 	email = prompt("What is the Customer's Email Address?", "@");
 	address = prompt("What is the Customer's Street Address?", "");
@@ -25,10 +27,27 @@ var newCustomer = function(){
 	state = prompt("What State?", "State");
 	zip = prompt("What is the zipcode?", "Zip");
 	customerAddress = new Array(address, city, state, zip);
-	var customer = new Array(firstName, lastName, birthDay.getMonth()+"/"+birthDay.getDate()+"/"+birthDay.getFullYear(), sex, race, height, phone, email, customerAddress);
+	var customer = new Array(firstName, lastName, customerBirthDay, sex, race, height, weight, phone, email, customerAddress);
 	console.log(customer);
 	return customer;
 }
 
 a = newCustomer();
-console.log(a);
+var firstLast = a[0] + " " + a[1];
+console.log("Customer: " + firstLast);
+var customerBday = a[2];
+console.log("Born: " + customerBday);
+var customerSex = a[3];
+console.log("Sex: " + customerSex);
+var customerRace = a[4];
+console.log("Race: " + customerRace);
+var customerHeight = a[5];
+console.log("Height: " + customerHeight);
+var customerWeight = a[6];
+console.log("Weight: " + customerWeight);
+var customerPhone = a[7];
+console.log("Phone Number: " + customerPhone);
+var customerEmail = a[8];
+console.log("Email: " + customerEmail);
+var currentAddress = a[9];
+console.log("Current Address:\n" + currentAddress[0] + "\n" + currentAddress[1] + ", " + currentAddress[2] + " " + currentAddress[3]);
