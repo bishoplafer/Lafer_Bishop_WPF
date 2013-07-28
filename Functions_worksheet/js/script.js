22bb0dc29978be76df948f97e59186263d2b754a
Bishop Lafer <bishoplafer@fullsail.edu>
2013-07-27 22:41:58 -0400

    Completed Functions Worksheet

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -2,3 +2,23 @@
 // Functions_worksheet
 // July 27, 2013
 
+// Circumference
+// C = 2PIr
+
+var calcCircumference = function(r){
+    var pi = Math.PI;
+    var circumference = 2 * pi * r;
+    return circumference;
+}
+var a = calcCircumference(5);
+console.log("The circumference of the circle is " + a);
+
+// Stung!
+var calcDeath = function(lbs){
+	beeStings = 8.666666667;
+	deathStings = lbs / beeStings;
+	return deathStings;
+}
+weight = 175;
+b = calcDeath(weight);
+console.log("It takes " + b + " beestings to kill a person who weighs " + weight + " lbs.");
